2891c60cda3984871494e77a59ce22a1da63a1fe
Sueda-Maya-Fukuyama <20010508@anabuki-net.ne.jp>
2021-07-09 15:05:55 +0900

    課題の22が出来ました。

--- ゲームプログラミング実習2021/ゲームプログラミング実習2021/GameProgramming/CXPlayer.cpp
+++ ゲームプログラミング実習2021/ゲームプログラミング実習2021/GameProgramming/CXPlayer.cpp
@@ -1,14 +1,23 @@
 #include "CXPlayer.h"
 #include "CKey.h"
+#include "CMatrix.h"
 
 void CXPlayer::Update(){
 	//Wキーが押されていれば歩き、押されていなければ待機
 	if (CKey::Push('W')){
 		ChangeAnimation(1, true, 60);
-		mPosition.mZ += 0.1f;
+		mPosition += CVector(0.0f, 0.0f, 0.1f)*mMatrixRotate;
 	}
 	else{
 		ChangeAnimation(0, true, 60);
+	}
+	//Aキーが押されていれば、Y軸の回転値に2加算する
+	if (CKey::Push('A')){
+		mRotation.mY += 2.0f;
+	}
+	//Dキーが押されていれば、Y軸の回転値を2減算する
+	if (CKey::Push('D')){
+		mRotation.mY -= 2.0f;
 	}
 	CXCharacter::Update();
 }
